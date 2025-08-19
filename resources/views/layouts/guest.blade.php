4cb0f4e5044b9f0f3cb5dec0b94cb5900f8b00a8
mohammed-abozamel112 <mohammed.abozamel112@gmail.com>
2025-08-19 16:41:20 +0300

    css styles

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,9 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ app()->getLocale()==='en'?'Fid Marketing Company' : 'شركة فيد للتسويق' }}</title>
-    <!-- Scripts -->
+    <title>{{ app()->getLocale() === 'en' ? 'Fid Marketing Company' : 'شركة فيد للتسويق' }}</title>
+    @if (app()->environment('local'))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <link rel="stylesheet" href="{{ asset('build/assets/app-Mf7hAoQL.css') }}">
+        <script type="module" src="{{ asset('build/assets/app-BY0LwmuQ.js') }}"></script>
+    @endif
 </head>
 
 <body class="font-sans antialiased">
