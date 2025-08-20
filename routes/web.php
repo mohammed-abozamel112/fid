49b54817ab28f2c219c36f4015ba23a11df7fa2c
mohammed-abozamel112 <mohammed.abozamel112@gmail.com>
2025-08-20 13:27:15 +0300

    contact

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,6 @@ Route::prefix('{lang}')->middleware(SetLocale::class)->group(function () {
     });
     //about view
     Route::view('about', 'about.index')->name('about');
-    Route::view('contact', 'contact.index')->name('contact');
+    Route::view('contact', 'contact.contact')->name('contact');
 
 });
